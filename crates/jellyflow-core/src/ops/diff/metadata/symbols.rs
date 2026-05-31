372be25b74cd63ba6479c35283f8f3c71c06af74
Latias94 <superfrankie621@gmail.com>
2026-06-01 02:39:16 +0800

    refactor(core): structure symbol diff fields

--- crates/jellyflow-core/src/ops/diff/metadata/symbols.rs
+++ crates/jellyflow-core/src/ops/diff/metadata/symbols.rs
@@ -1,44 +1,17 @@
 use super::super::GraphDiffPlanner;
+use crate::core::{Symbol, SymbolId};
 use crate::ops::GraphOp;
 
 impl<'a> GraphDiffPlanner<'a> {
     pub(crate) fn diff_symbols(&mut self) {
         let from = self.from;
         let to = self.to;
-        let tx = &mut self.tx;
 
         for (id, sym_to) in &to.symbols {
             if let Some(sym_from) = from.symbols.get(id) {
-                if sym_from.name != sym_to.name {
-                    tx.ops.push(GraphOp::SetSymbolName {
-                        id: *id,
-                        from: sym_from.name.clone(),
-                        to: sym_to.name.clone(),
-                    });
-                }
-                if sym_from.ty != sym_to.ty {
-                    tx.ops.push(GraphOp::SetSymbolType {
-                        id: *id,
-                        from: sym_from.ty.clone(),
-                        to: sym_to.ty.clone(),
-                    });
-                }
-                if sym_from.default_value != sym_to.default_value {
-                    tx.ops.push(GraphOp::SetSymbolDefaultValue {
-                        id: *id,
-                        from: sym_from.default_value.clone(),
-                        to: sym_to.default_value.clone(),
-                    });
-                }
-                if sym_from.meta != sym_to.meta {
-                    tx.ops.push(GraphOp::SetSymbolMeta {
-                        id: *id,
-                        from: sym_from.meta.clone(),
-                        to: sym_to.meta.clone(),
-                    });
-                }
+                self.diff_existing_symbol(*id, sym_from, sym_to);
             } else {
-                tx.ops.push(GraphOp::AddSymbol {
+                self.tx.ops.push(GraphOp::AddSymbol {
                     id: *id,
                     symbol: sym_to.clone(),
                 });
@@ -47,11 +20,42 @@ impl<'a> GraphDiffPlanner<'a> {
 
         for (id, sym_from) in &from.symbols {
             if !to.symbols.contains_key(id) {
-                tx.ops.push(GraphOp::RemoveSymbol {
+                self.tx.ops.push(GraphOp::RemoveSymbol {
                     id: *id,
                     symbol: sym_from.clone(),
                 });
             }
+        }
+    }
+
+    fn diff_existing_symbol(&mut self, id: SymbolId, sym_from: &Symbol, sym_to: &Symbol) {
+        if sym_from.name != sym_to.name {
+            self.tx.ops.push(GraphOp::SetSymbolName {
+                id,
+                from: sym_from.name.clone(),
+                to: sym_to.name.clone(),
+            });
+        }
+        if sym_from.ty != sym_to.ty {
+            self.tx.ops.push(GraphOp::SetSymbolType {
+                id,
+                from: sym_from.ty.clone(),
+                to: sym_to.ty.clone(),
+            });
+        }
+        if sym_from.default_value != sym_to.default_value {
+            self.tx.ops.push(GraphOp::SetSymbolDefaultValue {
+                id,
+                from: sym_from.default_value.clone(),
+                to: sym_to.default_value.clone(),
+            });
+        }
+        if sym_from.meta != sym_to.meta {
+            self.tx.ops.push(GraphOp::SetSymbolMeta {
+                id,
+                from: sym_from.meta.clone(),
+                to: sym_to.meta.clone(),
+            });
         }
     }
 }
