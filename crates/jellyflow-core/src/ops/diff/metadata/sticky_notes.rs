42ae6fd8b59b45ddc90efac6b2dfb92d367f2329
Latias94 <superfrankie621@gmail.com>
2026-06-01 02:40:55 +0800

    refactor(core): structure sticky note diff fields

--- crates/jellyflow-core/src/ops/diff/metadata/sticky_notes.rs
+++ crates/jellyflow-core/src/ops/diff/metadata/sticky_notes.rs
@@ -1,37 +1,17 @@
 use super::super::GraphDiffPlanner;
+use crate::core::{StickyNote, StickyNoteId};
 use crate::ops::GraphOp;
 
 impl<'a> GraphDiffPlanner<'a> {
     pub(crate) fn diff_sticky_notes(&mut self) {
         let from = self.from;
         let to = self.to;
-        let tx = &mut self.tx;
 
         for (id, note_to) in &to.sticky_notes {
             if let Some(note_from) = from.sticky_notes.get(id) {
-                if note_from.text != note_to.text {
-                    tx.ops.push(GraphOp::SetStickyNoteText {
-                        id: *id,
-                        from: note_from.text.clone(),
-                        to: note_to.text.clone(),
-                    });
-                }
-                if note_from.rect != note_to.rect {
-                    tx.ops.push(GraphOp::SetStickyNoteRect {
-                        id: *id,
-                        from: note_from.rect,
-                        to: note_to.rect,
-                    });
-                }
-                if note_from.color != note_to.color {
-                    tx.ops.push(GraphOp::SetStickyNoteColor {
-                        id: *id,
-                        from: note_from.color.clone(),
-                        to: note_to.color.clone(),
-                    });
-                }
+                self.diff_existing_sticky_note(*id, note_from, note_to);
             } else {
-                tx.ops.push(GraphOp::AddStickyNote {
+                self.tx.ops.push(GraphOp::AddStickyNote {
                     id: *id,
                     note: note_to.clone(),
                 });
@@ -40,11 +20,40 @@ impl<'a> GraphDiffPlanner<'a> {
 
         for (id, note_from) in &from.sticky_notes {
             if !to.sticky_notes.contains_key(id) {
-                tx.ops.push(GraphOp::RemoveStickyNote {
+                self.tx.ops.push(GraphOp::RemoveStickyNote {
                     id: *id,
                     note: note_from.clone(),
                 });
             }
+        }
+    }
+
+    fn diff_existing_sticky_note(
+        &mut self,
+        id: StickyNoteId,
+        note_from: &StickyNote,
+        note_to: &StickyNote,
+    ) {
+        if note_from.text != note_to.text {
+            self.tx.ops.push(GraphOp::SetStickyNoteText {
+                id,
+                from: note_from.text.clone(),
+                to: note_to.text.clone(),
+            });
+        }
+        if note_from.rect != note_to.rect {
+            self.tx.ops.push(GraphOp::SetStickyNoteRect {
+                id,
+                from: note_from.rect,
+                to: note_to.rect,
+            });
+        }
+        if note_from.color != note_to.color {
+            self.tx.ops.push(GraphOp::SetStickyNoteColor {
+                id,
+                from: note_from.color.clone(),
+                to: note_to.color.clone(),
+            });
         }
     }
 }
