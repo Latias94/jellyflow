8be0fa98f12ba578a2625f9976fe5b7f85ebb4e2
Latias94 <superfrankie621@gmail.com>
2026-06-01 03:33:32 +0800

    refactor(core): structure edge apply operations

--- crates/jellyflow-core/src/ops/apply/edges.rs
+++ crates/jellyflow-core/src/ops/apply/edges.rs
@@ -1,4 +1,4 @@
-use crate::core::Graph;
+use crate::core::{Edge, EdgeId, Graph, PortId};
 use crate::ops::GraphOp;
 
 use super::ApplyError;
@@ -6,71 +6,75 @@ use super::resources::remove_edge_exact;
 
 pub(super) fn apply_edge_op(graph: &mut Graph, op: &GraphOp) -> Result<(), ApplyError> {
     match op {
-        GraphOp::AddEdge { id, edge } => {
-            if graph.edges.contains_key(id) {
-                return Err(ApplyError::EdgeAlreadyExists { id: *id });
-            }
-            if !graph.ports.contains_key(&edge.from) {
-                return Err(ApplyError::EdgeMissingPort {
-                    edge: *id,
-                    port: edge.from,
-                });
-            }
-            if !graph.ports.contains_key(&edge.to) {
-                return Err(ApplyError::EdgeMissingPort {
-                    edge: *id,
-                    port: edge.to,
-                });
-            }
-            graph.edges.insert(*id, edge.clone());
-        }
+        GraphOp::AddEdge { id, edge } => apply_add_edge(graph, *id, edge)?,
         GraphOp::RemoveEdge { id, edge } => {
             remove_edge_exact(graph, *id, edge)?;
         }
         GraphOp::SetEdgeKind { id, to, .. } => {
-            let Some(edge) = graph.edges.get_mut(id) else {
-                return Err(ApplyError::MissingEdge { id: *id });
-            };
-            edge.kind = *to;
+            edge_mut(graph, *id)?.kind = *to;
         }
         GraphOp::SetEdgeSelectable { id, to, .. } => {
-            let Some(edge) = graph.edges.get_mut(id) else {
-                return Err(ApplyError::MissingEdge { id: *id });
-            };
-            edge.selectable = *to;
+            edge_mut(graph, *id)?.selectable = *to;
         }
         GraphOp::SetEdgeDeletable { id, to, .. } => {
-            let Some(edge) = graph.edges.get_mut(id) else {
-                return Err(ApplyError::MissingEdge { id: *id });
-            };
-            edge.deletable = *to;
+            edge_mut(graph, *id)?.deletable = *to;
         }
         GraphOp::SetEdgeReconnectable { id, to, .. } => {
-            let Some(edge) = graph.edges.get_mut(id) else {
-                return Err(ApplyError::MissingEdge { id: *id });
-            };
-            edge.reconnectable = *to;
+            edge_mut(graph, *id)?.reconnectable = *to;
         }
         GraphOp::SetEdgeEndpoints { id, to, .. } => {
-            let Some(edge) = graph.edges.get_mut(id) else {
-                return Err(ApplyError::MissingEdge { id: *id });
-            };
-            if !graph.ports.contains_key(&to.from) {
-                return Err(ApplyError::EdgeMissingPort {
-                    edge: *id,
-                    port: to.from,
-                });
-            }
-            if !graph.ports.contains_key(&to.to) {
-                return Err(ApplyError::EdgeMissingPort {
-                    edge: *id,
-                    port: to.to,
-                });
-            }
+            ensure_edge_exists(graph, *id)?;
+            ensure_edge_ports_exist(graph, *id, to.from, to.to)?;
+            let edge = edge_mut(graph, *id)?;
             edge.from = to.from;
             edge.to = to.to;
         }
         _ => unreachable!("non-edge op routed to edge apply"),
     }
     Ok(())
+}
+
+fn apply_add_edge(graph: &mut Graph, id: EdgeId, edge: &Edge) -> Result<(), ApplyError> {
+    if graph.edges.contains_key(&id) {
+        return Err(ApplyError::EdgeAlreadyExists { id });
+    }
+    ensure_edge_ports_exist(graph, id, edge.from, edge.to)?;
+    graph.edges.insert(id, edge.clone());
+    Ok(())
+}
+
+fn ensure_edge_exists(graph: &Graph, id: EdgeId) -> Result<(), ApplyError> {
+    if graph.edges.contains_key(&id) {
+        Ok(())
+    } else {
+        Err(ApplyError::MissingEdge { id })
+    }
+}
+
+fn ensure_edge_ports_exist(
+    graph: &Graph,
+    edge_id: EdgeId,
+    from: PortId,
+    to: PortId,
+) -> Result<(), ApplyError> {
+    if !graph.ports.contains_key(&from) {
+        return Err(ApplyError::EdgeMissingPort {
+            edge: edge_id,
+            port: from,
+        });
+    }
+    if !graph.ports.contains_key(&to) {
+        return Err(ApplyError::EdgeMissingPort {
+            edge: edge_id,
+            port: to,
+        });
+    }
+    Ok(())
+}
+
+fn edge_mut(graph: &mut Graph, id: EdgeId) -> Result<&mut Edge, ApplyError> {
+    graph
+        .edges
+        .get_mut(&id)
+        .ok_or(ApplyError::MissingEdge { id })
 }
